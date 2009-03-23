395f95b5dea3f657a46628f156102e017fc54d60
Andrea Reginato <andrew@mikamai.com>
2009-03-23 16:15:35 +0100

    Added capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "pickmi"
+
+set :deploy_to, "/var/apps/#{application}"
+
+set :repository,  "git@github.com:andreareginato/pickmi.git"
+set :scm, :git
+set :branch, "master"
+set :repository_cache, "git_master"
+set :git_enable_submodules, 1
+set :deploy_via, :remote_cache
+
+role :app, "pickmi.mikamai.com"
+role :web, "pickmi.mikamai.com"
+role :db,  "pickmi.mikamai.com", :primary => true
+
+set :user, "reggie"
+set :password, "andrea"
+
+set :use_sudo, false
+
+deploy.task :start do 
+end
+
+deploy.task :restart do
+  run "touch #{current_path}/tmp/restart.txt"
+end
