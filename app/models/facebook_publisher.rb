3c112e25f1d0e214afe476998b96c96c16cd383c
Vivien Schilis <vivien@mikamai.com>
2009-04-01 17:47:04 +0200

    missing files

--- app/models/facebook_publisher.rb
+++ app/models/facebook_publisher.rb
@@ -16,10 +16,10 @@ class FacebookPublisher < Facebooker::Rails::Publisher
     one_line_story_template("{*actor*} ha publicato i sui cinque libri favoriti")
 
     short_story_template("{*actor*} ha publicato i sui cinque libri favoriti",
-                    "Qui i cinque libri ")
+                          "Qui i cinque libri ")
 
     full_story_template("{*actor*} ha publicato i sui cinque libri favoriti",
-                      "Qui i cinque libri ")
+                          "Qui i cinque libri ")
 
   end 
   
@@ -30,16 +30,6 @@ class FacebookPublisher < Facebooker::Rails::Publisher
     attacker = Facebooker::User.new(user.uid) 
     send_as :user_action 
     from attacker
-    
-    # data({ 
-    #   :image_0 => image_tag (fb_user.books[0].photo, :width => '15'),
-    #   :image_1 => image_tag (fb_user.books[1].photo, :width => '15'),
-    #   :image_2 => image_tag (fb_user.books[2].photo, :width => '15'),
-    #   :image_3 => image_tag (fb_user.books[3].photo, :width => '15'),
-    #   :image_4 => image_tag (fb_user.books[4].photo, :width => '15')
-    # })
-    
-    logger.info("PHOOOOOOTO"+ image(fb_user.books.first.photo, root_url ).to_s)
     data :images => [ image(fb_user.books.first.photo, root_url) ]
 
     
